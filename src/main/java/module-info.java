2827437410243b717c0f574242de4f557bab3355
andu <alexandrugeorgepaval@gmail.com>
2026-01-06 19:27:31 +0200

    DB added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.musicappinjava {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
 
     opens com.example.musicappinjava to javafx.fxml;
     exports com.example.musicappinjava;
